269fdc2f15e25739ebefd66f85b4833081cfc51c
Peter Ihimire <peterihimire@gmail.com>
2021-03-01 16:33:08 +0100

    Added a context directory and file to manage our data on a global level , instead of using prop drilling . The context is the auth-context to manage user-login and logout

--- src/App.js
+++ src/App.js
@@ -17,45 +17,48 @@ import MainNavigation from "./shared/components/Navigation/MainNavigation";
 // import Auth from "./user/pages/Auth";
 import LoginPage from "./user/pages/LoginPage";
 import SignupPage from "./user/pages/SignupPage";
+import { AuthContext } from "./shared/context/auth-context";
 
 function App() {
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-          <Route path="/properties" exact>
-            <PropertiesPage />
-          </Route>
-          <Route path="/properties/new">
-            <NewProperty />
-          </Route>
-          <Route
-            path="/properties/:propertyId"
-            exact
-            component={UpdatePropertiesPage}
-          />
-          <Route
-            path="/properties/description/:slug"
-            exact
-            component={PropertyDescription}
-          />
-          <Route path="/users">
-            <UsersPage />
-          </Route>
-          <Route path="/users/:userId">
-            <SingleUser />
-          </Route>
-          {/* <Route path="/auth" component={Auth} /> */}
-          <Route path="/login" component={LoginPage} />
-          <Route path="/signup" component={SignupPage} />
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider>
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/properties" exact>
+              <PropertiesPage />
+            </Route>
+            <Route path="/properties/new">
+              <NewProperty />
+            </Route>
+            <Route
+              path="/properties/:propertyId"
+              exact
+              component={UpdatePropertiesPage}
+            />
+            <Route
+              path="/properties/description/:slug"
+              exact
+              component={PropertyDescription}
+            />
+            <Route path="/users">
+              <UsersPage />
+            </Route>
+            <Route path="/users/:userId">
+              <SingleUser />
+            </Route>
+            {/* <Route path="/auth" component={Auth} /> */}
+            <Route path="/login" component={LoginPage} />
+            <Route path="/signup" component={SignupPage} />
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
